69204aa813767b350edc3e259ad8573c74493264
gurxt <jung_jungian@outlook.com>
2023-10-07 12:20:09 -0400

    email verification complete for account creation

--- server/src/controllers/user_controller.ts
+++ server/src/controllers/user_controller.ts
@@ -4,6 +4,7 @@ import User from "#/models/user_model";
 import { generateToken } from '#/util/generateToken';
 import emailToken from '#/models/emailToken_model';
 import { sendVerificationMail } from '#/util/mail';
+import { IVerifyEmail } from '#/util/types/verifyEmail_types';
 
 export const createUser: RequestHandler = async (req: IUser, res: Response) => {
   const { username, email, password } = req.body;
@@ -18,4 +19,21 @@ export const createUser: RequestHandler = async (req: IUser, res: Response) => {
   });
 
   res.status(201).json({ user: { id: user._id, username, email }});
+}
+
+export const verifyEmail: RequestHandler = async (req: IVerifyEmail, res: Response) => {
+  const { token, userId } = req.body;
+  const verificationToken = await emailToken.findOne({ owner: userId });
+
+  if (!verificationToken) return res.status(403).json({ error: '[ERROR] invalid token.'});
+
+  const matched = await verificationToken.compareToken(token);
+
+  if (!matched) return res.status(403).json({ error: "[ERROR] Token does not match."})
+
+  await User.findByIdAndUpdate(userId, { verified: true })
+
+  await emailToken.findByIdAndDelete(verificationToken._id);
+
+  res.json({ message: "[INFO] your email is verified."});
 }
--- server/src/routers/auth_router.ts
+++ server/src/routers/auth_router.ts
@@ -1,10 +1,11 @@
-import { createUser } from '#/controllers/user_controller';
+import { createUser, verifyEmail } from '#/controllers/user_controller';
 import { validate } from '#/middleware/validator';
-import { CreateUserSchema } from '#/util/validation';
+import { CreateUserSchema, EmailVerificationSchema } from '#/util/validation';
 import { Router } from 'express';
 
 const router = Router();
 
 router.post('/create', validate(CreateUserSchema), createUser);
+router.post('/verify-email', validate(EmailVerificationSchema), verifyEmail);
 
 export default router;
